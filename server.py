aa80e877bc3f6a7c7d13d004758117d4fa4ee419
Ying Rong <ying9876543210@hotmail.com>
2022-03-06 11:25:09 -0800

    Initial commit

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "top secret"  
 
 @app.route('/')         
 def index():
@@ -7,8 +8,18 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['student_id'] = request.form['student_id']
+    session['value1'] = request.form['strawberry']
+    session['value2'] = request.form['raspberry']
+    session['value3'] = request.form['apple']
+    session['sum'] = int(request.form['apple'])+int(request.form['raspberry'])+int(request.form['strawberry'])
+    return redirect('/checkout')
+
+@app.route('/checkout')         
+def checkoutresult():
+    return render_template("checkout.html") 
 
 @app.route('/fruits')         
 def fruits():
